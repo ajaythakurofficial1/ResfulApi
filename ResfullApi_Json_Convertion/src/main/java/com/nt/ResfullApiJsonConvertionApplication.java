38ea78c0b9d2d35af3004ed653fd516d2621f3e1
ajaythakurofficial1 <165251043+ajaythakurofficial1@users.noreply.github.com>
2024-12-24 20:25:34 -0800

    Add files via upload

---
+++ ResfullApi_Json_Convertion/src/main/java/com/nt/ResfullApiJsonConvertionApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfullApiJsonConvertionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResfullApiJsonConvertionApplication.class, args);
+	}
+
+}
---
+++ ResfullApi_Json_Convertion/src/test/java/com/nt/ResfullApiJsonConvertionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfullApiJsonConvertionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
